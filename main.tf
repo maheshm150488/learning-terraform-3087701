c1e36d3fd31d384b20e83e956c178f430e18ee31
maheshm150488 <60420602+maheshm150488@users.noreply.github.com>
2023-08-15 14:37:19 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,5 @@
-data "aws_ami" "app_ami" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["979382823631"] # Bitnami
+provider "aws" {
+  region = "ap-southeast-2"
 }
 
 resource "aws_ssm_parameter" "HelloWorld" {
